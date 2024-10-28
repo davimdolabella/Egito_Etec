69da6b0b0144f4b58bdc6d9ff76cfb86feb9eab6
Davi Dolabella <davimdolabella03@gmail.com>
2024-10-27 21:34:59 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const nav = document.getElementById('piramide_img');
+window.addEventListener('wheel', function(event) {
+    let currentScroll = window.scrollY;
+    if (currentScroll < 15) {
+      event.preventDefault(); 
+      let scrollAmount = event.deltaY * 0.1;
+      let newScroll = currentScroll + scrollAmount;
+      window.scrollTo({
+        top: newScroll,  
+        behavior: 'smooth'
+      });
+    }
+  }, { passive: false });
+
+
+/*** Await ***/
+const awaitElements = document.querySelectorAll('.await');
+
+function applyToAllAwaitElements(callback) {
+    awaitElements.forEach(callback);
+}
+
+window.addEventListener("scroll", function() {
+    nav.classList.toggle('sticky', window.scrollY > 0);
+    applyToAllAwaitElements(element => {
+        if (window.scrollY < 1) {
+            element.style.opacity = 0;
+        } else {
+            element.style.opacity= 1;
+        }
+    });
+});
+
+
+
